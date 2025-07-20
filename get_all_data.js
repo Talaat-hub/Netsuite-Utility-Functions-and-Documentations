c7a01a6e7df9ecb6e39670c90807c4808ec968d2
Mahmoud Talaat <tmahmoud793@gmail.com>
2025-07-20 12:47:44 +0300

    Create get_all_data.js
    The maximum for search.create results are 1000 records after running the search, this function helps get all data even if more than 1000

---
+++ get_all_data.js
@@ -0,0 +1,18 @@
+const getAllData = (rs) => {
+    try {
+        const results = rs.run();
+        const searchResults = [];
+        let searchid = 0;
+        do {
+            var resultslice = results.getRange({ start: searchid, end: searchid + 1000 });
+            resultslice.forEach(function (slice) {
+                searchResults.push(slice);
+                searchid++;
+            }
+            );
+        } while (resultslice.length >= 1000);
+        return searchResults;
+    } catch (errGetAllData) {
+        log.debug('errGetAllData', errGetAllData)
+    }
+};
